908c8225cc661907a82b182e9459bcfaa94f6243
Christian Wang <christianwang39@gmail.com>
2026-01-10 12:57:55 -0800

    update

--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_course-material_page_actions_32a4460b.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/course-material/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_course-material_page_actions_32a4460b.js.map
--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"E1r/oQLqi5u1AnTLImbSqTdP8iwNITGbx7h5OI5Udjk=\"\n}"
